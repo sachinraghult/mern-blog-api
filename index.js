d03829173c167f8b44c25ea2ebd18440b75da8fa
sachinraghult <sachinraghult2002@gmail.com>
2022-02-05 13:42:00 +0530

    UPDATE : cors

--- index.js
+++ index.js
@@ -15,7 +15,30 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
-app.use(cors({ origin: true }));
+
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.configure(function () {
+    app.use(allowCrossDomain);
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(application_root, "public")));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
 
 
 // connecting to mongoDB
